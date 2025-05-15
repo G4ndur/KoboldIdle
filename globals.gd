59de368d4989fbc61b17e2c980f1933b78a993b1
Benito Schwenn <Schwennbenito@gmail.com>
2025-05-15 17:39:59 +0200

    Added first Generator and Global variables

---
+++ globals.gd
@@ -0,0 +1,15 @@
+extends Node
+var  GP = 10 
+#Manual Collecting
+var manualCollect = 1
+var manualCollectMult = 1
+var manualCollectCD = 1
+var manualCollectCDMult = 1
+var gpGenPerSec = 0
+var gpGenPerSecMult = 1
+#Kobolds
+var kobolds = 0
+var koboldPrice = 10
+var koboldGen = 1
+var koboldGenMult = 1
+var koboldGenPerSec = 0
--- main.gd
+++ main.gd
@@ -1,20 +1,48 @@
 extends Node2D
 class_name main
 
-var GP = 0
 func _ready() -> void:
 	$tick_timer.start()
 
 
 
 func _on_tick_timer_timeout() -> void:
-	$HUD.update_hoard(GP)
-	print($HUD/CollectButton/collect_cooldown.time_left)
+	$HUD.update_hoard(str(globals.GP).pad_decimals(0) )
+	#adds gp per sec to current gp
+	globals.GP += globals.koboldGenPerSec /5.0
+	
+	#Manual Collect Cooldown
+	
 	if $HUD/CollectButton/collect_cooldown.is_stopped() == false:
 		$HUD/CollectButton.disabled = true
+		$HUD/CollectButton.text = "Collect Shinies! \n" + str($HUD/CollectButton/collect_cooldown.time_left).pad_decimals(1)  
 	else:
 		$HUD/CollectButton.disabled = false
+		$HUD/CollectButton.text = "Collect Shinies! \n"
+		
+	#Unlocks
+	if globals.GP >= 10:
+		$HUD/HireKobold.visible = true
+	
+	
+	#button disable
+	if globals.GP < globals.koboldPrice:
+		$HUD/HireKobold.disabled = true
+	else:
+		$HUD/HireKobold.disabled = false
+	
 
 func _on_collect_button_pressed() -> void:
-	GP = GP + 1
-	$HUD/CollectButton/collect_cooldown.start(5)
+	globals.GP += globals.manualCollect * globals.manualCollectMult
+	$HUD/CollectButton/collect_cooldown.wait_time = globals.manualCollectCD * globals.manualCollectCDMult
+	$HUD/CollectButton/collect_cooldown.start()
+
+
+func _on_hire_kobold_pressed() -> void:
+	print("press")
+	globals.GP -= globals.koboldPrice
+	globals.koboldPrice *= 1.20
+	round(globals.koboldPrice)
+	$HUD/HireKobold.text = "Hire a Kobold\n"+ str(globals.koboldPrice).pad_decimals(0) + " GP"
+	globals.kobolds += 1
+	globals.koboldGenPerSec += globals.koboldGen * globals.koboldGenMult
